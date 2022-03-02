86d3e274d983b0629c1b4ae46a5f7b85cb3f7407
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-02 12:55:01 +0700

    show and hide a overlay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import { images } from "./components/Static/index";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showHandleCart = () => setCartIsShown(true);
+  const hideHandlerCart = () => setCartIsShown(false);
+
   return (
     <Fragment>
-      <Header src={images.meals} />
+      {cartIsShown ? <Cart hideShowCart={hideHandlerCart} /> : ""}
+      <Header src={images.meals} showCart={showHandleCart} />
       <main>
         <Meals />
       </main>
