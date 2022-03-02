387764ea23f9af72dfef59ab0dd3d4f79026ac0e
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-02 10:47:26 +0700

    add mealsummary jsx

---
+++ src/components/Meals/MealsSummary.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import style from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={style.summary}>
+      <h2>Delicious Food, Delivered for you</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+        vitae expedita alias nobis et eaque delectus, quae facilis error
+        eligendi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+        vitae expedita alias nobis et eaque delectus, quae facilis error
+        eligendi!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
