de5355c160728fcd99ae0ec903ead7a944eba67e
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-02 10:46:59 +0700

    add mealitem jsx

---
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import style from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
+
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={style.meal}>
+      <div>
+        <h3>{props.title}</h3>
+        <div className={style.description}>{props.desc}</div>
+        <div className={style.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.id}/>
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
