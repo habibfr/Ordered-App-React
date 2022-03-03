aef4077d70a84992f533af0d566524db62dc51c9
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-03 12:44:42 +0700

    animated useEffect

--- src/components/Layout/ButtonHeader.jsx
+++ src/components/Layout/ButtonHeader.jsx
@@ -1,18 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import style from "./ButtonHeader.module.css";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/Cart-context";
 
 const ButtonHeader = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnStyle = `${style.button} ${btnIsHighlighted ? style.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.onClick} className={style.button}>
+    <button onClick={props.onClick} className={btnStyle}>
       <span className={style.icon}>
         <CartIcon />
       </span>
