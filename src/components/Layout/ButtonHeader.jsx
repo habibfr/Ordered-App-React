5db3a56a41f9484321d1727a64b2a4228dc07495
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-03 11:21:54 +0700

    add useCtx

--- src/components/Layout/ButtonHeader.jsx
+++ src/components/Layout/ButtonHeader.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import style from "./ButtonHeader.module.css";
 
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/Cart-context";
 
 const ButtonHeader = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button onClick={props.onClick} className={style.button}>
       <span className={style.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={style.badge}>3</span>
+      <span className={style.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
